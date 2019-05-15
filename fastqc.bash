73a807be0242e76ee50064dd3641e8095542ed4f
Alessandro La Ferlita <45789805+alessandrolaferlita@users.noreply.github.com>
2019-05-15 10:22:52 +0200

    Update fastqc.bash

--- fastqc.bash
+++ fastqc.bash
@@ -11,37 +11,33 @@ while getopts ":f:s:o:" opt; do
 		f ) INPUT_1=$OPTARG ;;
 		s ) INPUT_2=$OPTARG ;;
 		o ) OUTPUT=$OPTARG ;;
-		\?) echo "Invalid option: -$OPTARG" >&2; exit 1 ;;			
+		\?) echo "Invalid option: -$OPTARG" >&2; exit 1 ;;
 		: ) echo "Option -$OPTARG requires an argument." >&2; exit 2;;
 	esac
 done
 
 #### Check parameters ####
-
 # Check input files
 if [ -z $INPUT_1 ] || [ ! -f $INPUT_1 ]; then
 	echo "Input file does not exist!" >&2
 	exit 3
 fi
-
 # Control sequencing strategy "single end" o "paired end"
 if [ -z $INPUT_2 ]; then
-	PAIRED=false
-elif [ ! -f $INPUT_2 ]; then 
+ PAIRED=false
+elif [ ! -f $INPUT_2 ]; then
 	echo "Second input file does not exist!" >&2
 	exit 4
 else
-    PAIRED=true
+ PAIRED=true
 fi
-
 # Check output
 if [ -z $OUTPUT ]; then
 	echo "Output file must be specified!" >&2
 	exit 5
 fi
-
 # Check if output directory is writable
-if [ ! -w $(dirname $OUTPUT) ]; then 
+if [ ! -w $(dirname $OUTPUT) ]; then
 	echo "Output directory is not writable!" >&2
 	exit 6
 fi
