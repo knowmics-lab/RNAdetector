a0b2eb4ac83696654c34c9d169c6a42a197128fd
Alessandro La Ferlita <45789805+alessandrolaferlita@users.noreply.github.com>
2019-07-18 11:22:58 +0200

    Create format_count_out.R

---
+++ format_count_out.R
@@ -0,0 +1,6 @@
+#### Change HTseq-count and FeatureCounts output files in the same format ####
+path <- "D:/NMSpipeline/read_counts/"
+counts_files_names <- list.files(path, pattern = "\\.txt")
+for (counts in counts_files_names){
+  if ()
+}
