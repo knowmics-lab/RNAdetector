b3c3400db99b11d7386eaa5628d6cd224ba8200a
Alessandro La Ferlita <45789805+alessandrolaferlita@users.noreply.github.com>
2019-09-12 11:17:01 +0200

    Update limma.R

--- limma.R
+++ limma.R
@@ -3,7 +3,7 @@
 # - Formatted raw counts input files path. Es. "D:/Human/proj_CLL_CD74_case_study/reads_quantification/prova/"
 # - Output path. Es. "D:/NMSpipeline/DE_results/limma_test.txt"
 # - Path sample info matrix. Es. "D:/Human/proj_CLL_CD74_case_study/reads_quantification/sample_info.txt"
-# 
+#
 ####################################################
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
@@ -29,20 +29,20 @@ limma_DE_analysis <- function(path_input, path_output, sample_info_path){
   }
   row.names(merged_raw_counts_data) <- merged_raw_counts_data$merged_raw_counts_data
   merged_raw_counts_data <- merged_raw_counts_data[,-c(1)]
-  
+
   # Differential expression analysis
   sample_info <- read_delim(sample_info_path, "\t", escape_double = FALSE, trim_ws = TRUE)
   design_matrix <- model.matrix(~0+sample_info$condition)
-  sample_types <- levels(factor(sample_info$condition)) 
+  sample_types <- levels(factor(sample_info$condition))
   colnames(design_matrix) <- sample_types
-  
+
   voomTransformed<-voom(merged_raw_counts_data, design_matrix, plot=FALSE)
   voomed.fitted <- lmFit(voomTransformed, design=design_matrix)
-  
+
   contrasts <- makeContrasts(sample-ctrl, levels = design_matrix)
   fit_2 <- contrasts.fit(voomed.fitted, contrasts)
   fit_2 <-eBayes(fit_2)
-  
+
   # Format output
   results <- topTable(fit_2, coef=1, number = Inf, adjust.method ="BH", sort.by="logFC")
   results <- results[,c(1,4,5)]
