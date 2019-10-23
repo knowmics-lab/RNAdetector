fe4080bca4a6d99df862b68bb56a95e99219f466
Alessandro La Ferlita <45789805+alessandrolaferlita@users.noreply.github.com>
2019-10-23 13:13:03 +0200

    Create salmon_index_2.sh

---
+++ salmon_index_2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+##############################################################################
+# Options:
+# 	-r FASTA transcripts files
+#   -i Indexed trascriptome (output folder)
+##############################################################################
+while getopts ":r:i:" opt; do
+	case $opt in
+		r ) FASTA=$OPTARG ;;
+		i ) INDEXED_FASTA=$OPTARG ;;
+		\?) echo "Invalid option: -$OPTARG" >&2; exit 1 ;;
+		: ) echo "Option -$OPTARG requires an argument." >&2; exit 2;;
+	esac
+done
+
+#### Check parameters ####
+# Check FASTA file
+if [ -z $FASTA ] || [ ! -f $FASTA ]; then
+	echo "FASTA file with transcripts does not exist!"
+	exit 3
+fi
+
+#### Indexed transcriptome ####
+sudo docker run -v `pwd`:`pwd` -w `pwd` combinelab/salmon salmon index -t $FASTA -i $INDEXED_FASTA -k 31
+
+if [ ! -d $INDEXED_FASTA ]; then
+	echo "Indexed trascriptome does not exist!"
+	exit 4
+fi
