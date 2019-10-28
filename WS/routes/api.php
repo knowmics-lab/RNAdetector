55137ea7d563ef2a7e3bda07dae9adae7f6ac6c0
Salvatore Alaimo <alaimos@gmail.com>
2019-10-28 12:33:32 +0100

    Update api.php

--- WS/routes/api.php
+++ WS/routes/api.php
@@ -13,27 +13,38 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::apiResource('users', 'Api\\UserController')->names([
-    'show' => 'users.show',
-])->middleware('auth:api');
+Route::apiResource('users', 'Api\\UserController')->names(
+    [
+        'show' => 'users.show',
+    ]
+)->middleware('auth:api');
 
-Route::middleware([
-    'auth:api',
-    'can:generate-token,user',
-])->get('/users/{user}/token', 'Api\\UserController@token');
+Route::middleware(
+    [
+        'auth:api',
+        'can:generate-token,user',
+    ]
+)->get('/users/{user}/token', 'Api\\UserController@token');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth:api')->get(
+    '/user',
+    function (Request $request) {
+        return $request->user();
+    }
+);
 
-Route::apiResource('jobs', 'Api\\JobController')->names([
-    'show' => 'jobs.show',
-])->middleware('auth:api');
+Route::apiResource('jobs', 'Api\\JobController')->names(
+    [
+        'show' => 'jobs.show',
+    ]
+)->middleware('auth:api');
 
-Route::middleware([
-    'auth:api',
-    'can:submit-job,job',
-])->get('/jobs/{job}/submit', 'Api\\JobController@submit')->name('jobs.submit');
+Route::middleware(
+    [
+        'auth:api',
+        'can:submit-job,job',
+    ]
+)->get('/jobs/{job}/submit', 'Api\\JobController@submit')->name('jobs.submit');
 
 Route::middleware('auth:api')->get('/job-types', 'Api\\JobTypeController@index')->name('job-types.index');
 Route::middleware('auth:api')->get('/job-types/{type}', 'Api\\JobTypeController@show')->name('job-types.show');
