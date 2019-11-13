b4c10cfcbca45f1c38130953253356c505ba15dc
Alessandro La Ferlita <45789805+alessandrolaferlita@users.noreply.github.com>
2019-11-13 10:11:10 +0100

    Create SmallRnaJobType.php

---
+++ WS/app/Jobs/Types/SmallRnaJobType.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * RNADetector Web Service
+ *
+ * @author A. La Ferlita, Ph.D. Student <alessandrolf90 at hotmail dot it>
+ */
+
+namespace App\Jobs\Types;
+
+
+use App\Exceptions\ProcessingJobException;
+use App\Jobs\Types\Traits\ConvertsBamToFastqTrait;
+use App\Jobs\Types\Traits\RunTrimGaloreTrait;
+use App\Utils;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Storage;
+
+class SmallRnaJobType extends AbstractJob
+{
+
+    /**
+     * Returns an array containing for each input parameter an help detailing its content and use.
+     *
+     * @return array
+     */
+    public static function parametersSpec(): array
+    {
+        return [
+            'name' => 'An optional string containing a name',
+        ];
+    }
+
+    /**
+     * Returns an array containing for each output value an help detailing its use.
+     *
+     * @return array
+     */
+    public static function outputSpec(): array
+    {
+        return [
+            'greetings' => 'A greeting to the user',
+        ];
+    }
+
+    /**
+     * Returns an array containing rules for input validation.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return array
+     */
+    public static function validationSpec(Request $request): array
+    {
+        return [
+            'name' => ['filled', 'min:2', 'max:20'],
+        ];
+    }
+
+    /**
+     * Checks the input of this job and returns true iff the input contains valid data
+     * The default implementation does nothing.
+     *
+     * @return bool
+     */
+    public function isInputValid(): bool
+    {
+        return true;
+    }
+
+
+    /**
+     * Handles all the computation for this job.
+     * This function should throw a ProcessingJobException if something went wrong during the computation.
+     * If no exceptions are thrown the job is considered as successfully completed.
+     *
+     * @throws \App\Exceptions\ProcessingJobException
+     */
+    public function handle(): void
+    {
+        try {
+            $name = $this->model->getParameter('name', \Auth::user()->name);
+            $this->model->setOutput('greetings', 'Hello ' . $name . '!!');
+        } catch (\Exception $e) {
+            throw new ProcessingJobException('An error occurred during job processing.', 0, $e);
+        }
+    }
+
+    /**
+     * Returns a description for this job
+     *
+     * @return string
+     */
+    public static function description(): string
+    {
+        return 'A greeting to the user';
+    }
+}
