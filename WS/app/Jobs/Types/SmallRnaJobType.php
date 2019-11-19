0157d8d79f30a63552b0f9a30e2feafd320e0ecc
Salvatore Alaimo <alaimos@gmail.com>
2019-11-19 09:54:36 +0100

    Updated SmallRNA Job with new models

--- WS/app/Jobs/Types/SmallRnaJobType.php
+++ WS/app/Jobs/Types/SmallRnaJobType.php
@@ -11,6 +11,8 @@ namespace App\Jobs\Types;
 use App\Exceptions\ProcessingJobException;
 use App\Jobs\Types\Traits\ConvertsBamToFastqTrait;
 use App\Jobs\Types\Traits\RunTrimGaloreTrait;
+use App\Models\Annotation;
+use App\Models\Reference;
 use App\Utils;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
@@ -20,9 +22,12 @@ class SmallRnaJobType extends AbstractJob
 {
     use ConvertsBamToFastqTrait, RunTrimGaloreTrait;
 
-    private const FASTQ             = 'fastq';
-    private const BAM               = 'BAM';
-    private const VALID_INPUT_TYPES = [self::FASTQ, self::BAM];
+    private const FASTQ                = 'fastq';
+    private const BAM                  = 'BAM';
+    private const HTSEQ_COUNTS         = 'htseq';
+    private const FEATURECOUNTS_COUNTS = 'feature-counts';
+    private const VALID_INPUT_TYPES    = [self::FASTQ, self::BAM];
+    private const VALID_COUNTS_METHOS  = [self::HTSEQ_COUNTS, self::FEATURECOUNTS_COUNTS];
 
     /**
      * Returns an array containing for each input parameter an help detailing its content and use.
@@ -32,20 +37,20 @@ class SmallRnaJobType extends AbstractJob
     public static function parametersSpec(): array
     {
         return [
-            'paired'               => 'A boolean value to indicate whether sequencing strategy is paired-ended or not (Default false)',
-            'firstInputFile'       => 'Required, input file for the analysis',
-            'secondInputFile'      => 'Required if paired is true and inputType is fastq. The second reads file',
-            'inputType'            => 'Required, type of the input file (fastq, bam)',
-            'convertBam'           => 'If inputType is bam converts input in another format: fastq.',
-            'trimGalore'           => [
+            'paired'            => 'A boolean value to indicate whether sequencing strategy is paired-ended or not (Default false)',
+            'firstInputFile'    => 'Required, input file for the analysis',
+            'secondInputFile'   => 'Required if paired is true and inputType is fastq. The second reads file',
+            'inputType'         => 'Required, type of the input file (fastq, bam)',
+            'convertBam'        => 'If inputType is bam converts input in another format: fastq.',
+            'trimGalore'        => [
                 'enable'  => 'A boolean value to indicate whether trim galore should run (This parameter works only for fastq files)',
                 'quality' => 'Minimal PHREAD quality for trimming (Default 20)',
                 'length'  => 'Minimal reads length (Default 14)',
             ],
-            'customGTFFile'        => 'An optional GTF file for custom annotation of reads (Not needed for human hg19)',
-            'customGenomeName'     => 'An optional name for the custom genome',
-            'customFASTAGenome'    => 'An optional Genome to employ for custom annotation (Not needed for human hg19)',
-            'threads'              => 'Number of threads for this analysis (Default 1)',
+            'countingAlgorithm' => 'The counting algorithm htseq or feature-counts (Default htseq)',
+            'genome'            => 'An optional name for a reference genome (Default human hg19)',
+            'annotation'        => 'An optional name for a genome annotation (Default human hg19)',
+            'threads'           => 'Number of threads for this analysis (Default 1)',
         ];
 
     }
@@ -72,9 +77,9 @@ class SmallRnaJobType extends AbstractJob
     public static function validationSpec(Request $request): array
     {
         return [
-            'paired'               => ['filled', 'boolean'],
-            'firstInputFile'       => ['required', 'string'],
-            'secondInputFile'      => [
+            'paired'             => ['filled', 'boolean'],
+            'firstInputFile'     => ['required', 'string'],
+            'secondInputFile'    => [
                 Rule::requiredIf(
                     static function () use ($request) {
                         return $request->get('parameters.inputType') === self::FASTQ && ((bool)$request->get(
@@ -85,16 +90,16 @@ class SmallRnaJobType extends AbstractJob
                 ),
                 'string',
             ],
-            'inputType'            => ['required', Rule::in(self::VALID_INPUT_TYPES)],
-            'convertBam'           => ['filled', 'boolean'],
-            'trimGalore'           => ['filled', 'array'],
-            'trimGalore.enable'    => ['filled', 'boolean'],
-            'trimGalore.quality'   => ['filled', 'integer'],
-            'trimGalore.length'    => ['filled', 'integer'],
-            'customGTFFile'        => ['filled', 'string'],
-            'customGenomeName'     => ['filled', 'alpha_num'],
-            'customFASTAGenome'    => ['filled', 'string'],
-            'threads'              => ['filled', 'integer'],
+            'inputType'          => ['required', Rule::in(self::VALID_INPUT_TYPES)],
+            'convertBam'         => ['filled', 'boolean'],
+            'trimGalore'         => ['filled', 'array'],
+            'trimGalore.enable'  => ['filled', 'boolean'],
+            'trimGalore.quality' => ['filled', 'integer'],
+            'trimGalore.length'  => ['filled', 'integer'],
+            'countingAlgorithm'  => ['filled', Rule::in(self::VALID_COUNTS_METHOS)],
+            'genome'             => ['filled', 'alpha_dash', Rule::exists('references', 'name')],
+            'annotation'         => ['filled', 'alpha_dash', Rule::exists('annotations', 'name')],
+            'threads'            => ['filled', 'integer'],
         ];
     }
 
@@ -110,9 +115,11 @@ class SmallRnaJobType extends AbstractJob
         $inputType = $this->model->getParameter('inputType');
         $firstInputFile = $this->model->getParameter('firstInputFile');
         $secondInputFile = $this->model->getParameter('secondInputFile');
-        $customGTFFile = $this->model->getParameter('customGTFFile');
-        $customFASTAGenome = $this->model->getParameter('customFASTAGenome');
+        $countingAlgorithm = $this->model->getParameter('countingAlgorithm', self::HTSEQ_COUNTS);
         if (!in_array($inputType, self::VALID_INPUT_TYPES, true)) {
+            return false;
+        }
+        if (!in_array($countingAlgorithm, self::VALID_COUNTS_METHOS, true)) {
             return false;
         }
         $disk = Storage::disk('public');
@@ -125,92 +132,52 @@ class SmallRnaJobType extends AbstractJob
                 ))) {
             return false;
         }
-        if (!empty($customGTFFile) && !$disk->exists($dir . $customGTFFile)) {
-            return false;
-        }
-        if (!empty($customFASTAGenome) && !$disk->exists($dir . $customFASTAGenome)) {
-            return false;
-        }
+
         return true;
     }
 
     /**
      * Runs TopHat
      *
-     * @param bool        $paired
-     * @param string      $firstInputFile
-     * @param string|null $secondInputFile
-     * @param int         $threads
-     * @param string|null $customGTFFile
-     * @param string|null $customFASTAGenome
-     * @param string|null $customGenomeName
+     * @param bool                   $paired
+     * @param string                 $firstInputFile
+     * @param string|null            $secondInputFile
+     * @param \App\Models\Reference  $genome
+     * @param \App\Models\Annotation $annotation
+     * @param int                    $threads
      *
      * @return string
      * @throws \App\Exceptions\ProcessingJobException
      */
-
     private function runTophat(
         bool $paired,
         string $firstInputFile,
         ?string $secondInputFile,
-        int $threads = 1,
-        ?string $customGTFFile = null,
-        ?string $customFASTAGenome = null,
-        ?string $customGenomeName = null
-    ): string
-    {
-        if ($customGTFFile === null) {
-            $customGTFFile = env('HUMAN_GTF_PATH');
-            $this->log('Using Human genome annotation.');
-        }else{$this->log('Using custom genome annotation.');}
-        $index = false;
-        if ($customFASTAGenome === null) {
-            $genomeDir = env('HUMAN_BOWTIE_GENOME');
-            $this->log('Using Human genome.');
-        } else {
-            $genomeDir = env('CUSTOM_GENOME_PATH') . '/' . $customGenomeName;
-            if (!file_exists($genomeDir) || !is_dir($genomeDir)) {
-                $index = true;
-            }else{$this->log('Using previously indexed genome.');}
-        }
-        if ($index) {
-            $this->log('Indexing custom genome');
-            // Call Bowtie index script
-            $this->log('Custom genome indexed');
-            if (!file_exists($genomeDir) && !is_dir($genomeDir)) {
-                throw new ProcessingJobException('Unable to create indexed genome');
-            }
-        }
+        Reference $genome,
+        Annotation $annotation,
+        int $threads = 1
+    ): string {
         $bamOutput = $this->model->getJobTempFileAbsolute('bowtie_output', '.bam');
         // Call TopHat
         if (!file_exists($bamOutput)) {
             throw new ProcessingJobException('Unable to create TopHat output file');
         }
+
         return $bamOutput;
     }
 
     /**
-     *
      * Runs HTseq-count
      *
-     * @param string $htseqInputFile
-     * @param string|null $customGTFFile
-     * @param int $threads
+     * @param string                 $htseqInputFile
+     * @param \App\Models\Annotation $annotation
+     * @param int                    $threads
+     *
      * @return array
-     * @throws ProcessingJobException
+     * @throws \App\Exceptions\ProcessingJobException
      */
-
-    private function runHTSEQ(
-        string $htseqInputFile,
-        ?string $customGTFFile = null,
-        int $threads = 1
-    ): array {
-        if ($customGTFFile === null) {
-            $customGTFFile = env('HUMAN_GTF_PATH');
-            $this->log('Using Human genome annotation.');
-        } else {
-            $this->log('Using custom genome annotation.');
-        }
+    private function runHTSEQ(string $htseqInputFile, Annotation $annotation, int $threads = 1): array
+    {
         $htseqOutputRelative = $this->model->getJobTempFile('htseq_output', '.txt');
         $htseqOutput = $this->model->absoluteJobPath($htseqOutputRelative);
         $htseqOutputUrl = \Storage::disk('public')->url($htseqOutput);
@@ -222,18 +189,18 @@ class SmallRnaJobType extends AbstractJob
         return [$htseqOutputRelative, $htseqOutputUrl];
     }
 
-
-    private function runFeatureCount(
-        string $featurecountInputFile,
-        ?string $customGTFFile = null,
-        int $threads = 1
-    ): array {
-        if ($customGTFFile === null) {
-            $customGTFFile = env('HUMAN_GTF_PATH');
-            $this->log('Using Human genome annotation.');
-        } else {
-            $this->log('Using custom genome annotation.');
-        }
+    /**
+     * Runs FeatureCount
+     *
+     * @param string                 $featurecountInputFile
+     * @param \App\Models\Annotation $annotation
+     * @param int                    $threads
+     *
+     * @return array
+     * @throws \App\Exceptions\ProcessingJobException
+     */
+    private function runFeatureCount(string $featurecountInputFile, Annotation $annotation, int $threads = 1): array
+    {
         $featurecountOutputRelative = $this->model->getJobTempFile('featurecount_output', '.txt');
         $featurecountOutput = $this->model->absoluteJobPath($featurecountOutputRelative);
         $featurecountOutputUrl = \Storage::disk('public')->url($featurecountOutput);
