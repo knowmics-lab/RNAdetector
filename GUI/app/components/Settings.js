e942d8dae16525d3921311386d29663df530d362
Salvatore Alaimo <alaimos@gmail.com>
2019-12-10 11:49:07 +0100

    GUI: working on the settings panel

--- GUI/app/components/Settings.js
+++ GUI/app/components/Settings.js
@@ -3,15 +3,28 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Typography } from '@material-ui/core';
 import { Formik } from 'formik';
+import * as Yup from 'yup';
 
 type Props = {};
 
 export default class Settings extends Component<Props> {
   props: Props;
 
+  formSubmit = (values, helpers) => {
+    console.log(values);
+  }
+
   render() {
+    const validationSchema = Yup.object().shape({
+
+    });
     return (
       <div>
+        <Formik initialValues=[{}] validationSchema={validationSchema} onSubmit={this.formSubmit}>
+          {({ errors, touched, isValidating}) => {
+
+          }}
+        </Formik>
         <Typography paragraph>Settings</Typography>
       </div>
     );
