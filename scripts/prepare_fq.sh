2892a65acd69409136b3c520a09a107467d93e10
Salvatore Alaimo <alaimos@gmail.com>
2020-02-01 17:10:41 +0100

    Create prepare_fq.sh

---
+++ scripts/prepare_fq.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+INPUT="${1}"
+
+if [ -z $INPUT ]; then
+  echo "Input file is required"
+  exit 1
+fi
+
+if [ ! -f $INPUT ]; then
+  echo "Input file does not exist."
+  exit 2
+fi
+
+MIME=$(file -i -0 "$INPUT" | cut -f 2 -d " " | cut -f 1 -d ";")
+EXTENSION="${INPUT_NAME##*.}"
+FILENAME="$(dirname "$INPUT")/$(basename "$INPUT" ".${EXTENSION}")"
+
+COMPRESSED=false
+if [ "$MIME" = "application/gzip" ]; then
+  echo "Extracting input file"
+  gunzip -v "$INPUT"
+  COMPRESSED=true
+elif [ "$MIME" = "application/x-bzip2" ]; then
+  echo "Extracting input file"
+  bunzip2 -v "$INPUT"
+  COMPRESSED=true
+fi
+
+if [ "$COMPRESSED" = "true" ]; then
+  if [ ! -f "$FILENAME" ]; then
+    echo "Unable to find extracted file"
+    exit 3
+  fi
+fi
