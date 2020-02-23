ff0808d1e3659eecad1146917352256726898e63
Salvatore Alaimo <alaimos@gmail.com>
2020-02-23 11:27:08 +0100

    Upgrade docker

--- scripts/base/installPackages.R
+++ scripts/base/installPackages.R
@@ -1,4 +1,9 @@
 #!/usr/bin/env Rscript
 
 install.packages("BiocManager")
-BiocManager::install(c("limma", "edgeR", "DESeq2", "readr", "tximport", "optparse", "dplyr", "rtracklayer", "plyr"),ask = FALSE)
+BiocManager::install(c("limma", "edgeR", "DESeq2", "readr", "tximport", 
+                        "optparse", "dplyr", "rtracklayer", "plyr", 
+                        "survcomp", "VennDiagram", "knitr", "zoo",
+                        "devtools"),ask = FALSE)
+
+remotes::install_github("alaimos/metaseqR", dependencies = TRUE, upgrade = "always")
