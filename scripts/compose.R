58b25c6894d3bab979d36c22716c5f5059ba20ec
Salvatore Alaimo <alaimos@gmail.com>
2020-05-25 22:14:29 +0200

    Update compose.R

--- scripts/compose.R
+++ scripts/compose.R
@@ -53,19 +53,15 @@ merge.ciri <- function (files, bedfiles, mapfiles) {
     juncread[tmp, ifile] <- glist[[ifile]]$juncread
   }
   juncread <- as.data.frame(juncread)
-  print(length(gcirc$strand))
-  print(length(width(ranges(gcirc))))
-  s <- start(ranges(gcirc))
-  e <- end(ranges(gcirc))
   return (data.frame(
     id=gcirc$id,
     mapped_id=gcirc$mapped_id,
     name=gcirc$id,
     chr=as(seqnames(gcirc), "vector"),
-    start=s,
-    end=e,
-    strand=gcirc$strand,
-    length=s-e+1,
+    start=start(ranges(gcirc)),
+    end=end(ranges(gcirc)),
+    strand=as.vector(strand(gcirc)),
+    length=width(ranges(gcirc)),
     juncread
   ))
 }
