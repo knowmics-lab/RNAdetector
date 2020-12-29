b34b5cbac40974071ba0f246c28697b01d946f73
Salvatore Alaimo <alaimos@gmail.com>
2020-12-29 12:45:40 +0100

    Pathway Analysis bugfixes

--- scripts/base/installPackages.R
+++ scripts/base/installPackages.R
@@ -5,7 +5,7 @@ BiocManager::install(c("limma", "edgeR", "DESeq2", "readr", "tximport",
                         "optparse", "dplyr", "rtracklayer", "plyr", 
                         "survcomp", "VennDiagram", "knitr", "zoo",
                         "devtools", "plotly", "rmarkdown", "DT",
-                        "heatmaply"),ask = FALSE)
+                        "heatmaply", "shiny"),ask = FALSE)
 
 devtools::install_github("rstudio/d3heatmap", dependencies = TRUE, upgrade = "always")
 remotes::install_github("alaimos/metaseqR", dependencies = TRUE, upgrade = "always")
