4cb60eb9bd305eef0dc44b3b938502cdb8a82ec5
Salvatore Alaimo <alaimos@gmail.com>
2021-07-01 10:06:24 +0200

    Build script now works with branches

--- scripts/base/build.sh
+++ scripts/base/build.sh
@@ -3,6 +3,11 @@
 if ! git clone https://github.com/alessandrolaferlita/RNAdetector.git; then
     exit 1
 fi
+if [ ! -z "$1" ]; then
+    cd ./RNAdetector
+    git checkout "$1"
+    cd ..
+fi
 tar -zcvf repo.tar.gz --owner=0 --group=0 ./RNAdetector
 rm -rf ./RNAdetector
 docker build -t alaimos/rnadetector:v0.0.3 .
